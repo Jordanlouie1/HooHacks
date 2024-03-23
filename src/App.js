2b3d6bc0728dc8b9edec5d76d6cafb7ad0acb16e
Funmiso Adeniyi <funmiso4@gmail.com>
2024-03-23 19:47:51 -0400

    Funmiso Title Push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TitlePage from './TitlePage';
+import FindJobsButton from './FindJobButtons'; // Import your FindJobsPage component here
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<TitlePage />} />
+          <Route path="/find-jobs" element={<FindJobsButton />} />
+        </Routes>
+      </div>
+    </Router>
   );
-
-    
-
 }
-
-export default App;
---
+++ src/FindJobButtons.js
@@ -0,0 +1,10 @@
+// FindJobsButton.js
+import React from 'react';
+
+function FindJobsButton() {
+  return (
+    <button className="find-jobs-button">Find Jobs</button>
+  );
+}
+
+export default FindJobsButton;
